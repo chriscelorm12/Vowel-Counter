d241598822710351d9a38a305f5ef87b1089ef9c
chriscelorm12 <redemption256@gmail.com>
2024-02-19 12:13:51 +0000

    vowel counter

---
+++ script.js
@@ -0,0 +1,23 @@
+function isVowel(char) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    return vowels.includes(char)
+}
+
+function checkVowels() {
+    let text = document.getElementById('inputText').value;
+    let vowelCount = 0;
+    text = textLowerCase()
+
+    for (let i = 0; i < text.length; i++) {
+        let char = text.charAt(i);
+
+        if (isVowel(char)) {
+            vowelCount++;
+        }
+    }
+
+    const result = document.getElementById('result');
+    result.textContent = 'Total Vowels: ' + vowelCount;
+}
+
